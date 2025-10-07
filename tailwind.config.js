d55d43653291dcd1ace45b15329273fc429f1e99
joelrubio06 <joelrubio@iesmontsia.org>
2025-10-07 16:36:12 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'fons-pagina': '#C2B098',
+        'fons-login': '#ffffff',
+        'color-titol': '#FF7043',
+        'color-titol-hover': '#BF4519',
+        'fons-input': '#FFE6CC',
+        'bordi-input': '#1F2937',
+        'text-input': '#000000',
+        'focus-input': '#FFD8A8',
+        'text-principal': '#000000',
+        'color-enllaç': '#FF7043',
+      },
+    },
+  },
+  plugins: [],
+}
+
